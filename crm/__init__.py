9a85e8fd6ab7511c5fd5fbb9832a91365f98cd5e
Mitja Karhusaari <mitja@karhusaari.me>
2021-08-04 06:01:46 +0300

    Override config using DATABASE_URL environment variable

--- crm/__init__.py
+++ crm/__init__.py
@@ -3,6 +3,7 @@ import toml
 
 from sqlalchemy import select
 
+import os
 import crm.settings
 import crm.auth
 import crm.db
@@ -17,7 +18,9 @@ def create_app(test_config=None):
     app = Flask(__name__)
 
     app.config.from_file('config.toml', load=toml.load)
-    # app.config.from_envvar('CRM_CONFIG', load=toml.load)
+
+    if 'DATABASE_URL' in os.environ:
+        app.config['DATABASE_URL'] = os.environ['DATABASE_URL']
 
     db.init_app(app)
 
